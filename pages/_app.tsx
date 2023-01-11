e809a4c17519fe52bd3de119e398e263417aaebb
Jason Savage <savage@revlogical.com>
2023-01-11 16:30:40 -0600

    FBP

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,22 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-
-import * as gtag from "../lib/gtag";
 import Script from "next/script";
 import Head from "next/head";
+
+import * as gtag from "../lib/gtag";
+import * as fbq from "../lib/fpixel";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
+    fbq.pageview();
     const handleRouteChange = (url) => {
       gtag.pageview(url);
+      fbq.pageview();
     };
     router.events.on("routeChangeComplete", handleRouteChange);
     router.events.on("hashChangeComplete", handleRouteChange);
@@ -28,6 +31,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>RND KC</title>
         <link rel="shortcut icon" href="/img/RNDBlack2.svg" />
         <link rel="icon" href="/img/RNDBlack2.svg" />
+        <meta
+          name="facebook-domain-verification"
+          content="5yr3moibq3quc8mlhc65p4ore1426a"
+        />
         <style>
           @import
           url("https://fonts.googleapis.com/css2?family=Akshar:wght@300;400;500;600;700&display=swap");
@@ -54,6 +61,25 @@ function MyApp({ Component, pageProps }: AppProps) {
             gtag('config', '${gtag.GA_TRACKING_ID}', {
               page_path: window.location.pathname,
             });
+          `,
+        }}
+      />
+      {/* Global Site Code Pixel - Facebook Pixel */}
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', ${fbq.FB_PIXEL_ID});
+            fbq('track', 'PageView');
           `,
         }}
       />
