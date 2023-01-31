a4bacb71ac6e835ddf68c60323027a89bfed7fbc
Jason Savage <savage@revlogical.com>
2023-01-31 08:28:26 -0600

    fix header

--- components/hero.tsx
+++ components/hero.tsx
@@ -10,20 +10,47 @@ export const Hero = () => {
       <section id="hero" className="hero">
         <article className="hero-header">
           <h1 id="home" className="hero-header--title">
-            Owning a classic car is hard,
+            Money can't buy happiness...
           </h1>
-          <article className="hero-header-sub">
-            <h2 className="hero-header--subtitle">renting one is easy as</h2>
-            <img src="./img/RNDBlack2.svg" className="hero-header--logo" />
+          <h2 className="hero-header--subtitle">
+            but you can rent it for <span className="text-accent">$350.</span>
+          </h2>
+          <NewsLetterSignUpForm justify="start" />
+        </article>
+        <article className="hero-images">
+          <Image
+            src="/img/67Txt.svg"
+            priority
+            alt="Camaro"
+            width="1900"
+            height="100"
+            className="hero-images--text"
+          />
+          <Image
+            src="/img/67CamaroTxt.svg"
+            priority
+            alt="Camaro"
+            width="1900"
+            height="100"
+            className="hero-images--text-full"
+          />
+          <article className="hero-images--camaro-box">
+            <img
+              src="/img/CamaroImg.png"
+              alt="67"
+              className="hero-images--camaro"
+            />
+            <article className="hero-social">
+              <Social
+                bgColor="black"
+                fgColor="white"
+                text="black"
+                justify="center"
+              />
+            </article>
           </article>
         </article>
       </section>
-      <article className="hero-newsletter">
-        <h2 className="hero-newsletter_heading">
-          Sign up now for discounts and special offers
-        </h2>
-        <NewsLetterSignUpForm justify="start" text="Sign Up" />
-      </article>
       <Divider light />
     </section>
   );
