7ae86db88ff9a9ceffa7dfbd7084f5f65de6dcd5
Jason Savage <savage@revlogical.com>
2023-02-13 11:36:49 -0600

    Minor bug

--- components/comingSoonImageGrid.tsx
+++ components/comingSoonImageGrid.tsx
@@ -4,7 +4,7 @@ export const ComingSoonImageGrid = () => {
   return (
     <section className="rental-ready_image-grid">
       <ImageGridItem
-        src="/img/inventory/58 Corvette/58vette3.jpg"
+        src="/img/car.svg"
         alt="1958 Chevy Corvette"
         title="1958 Chevy Corvette"
         go
@@ -13,14 +13,14 @@ export const ComingSoonImageGrid = () => {
       />
       <ImageGridItem
         src="/img/inventory/59 Austin Healy/ah4.jpg"
-        alt="1959 Austin Healy Mark 1"
-        title="1959 Austin Healy Mark 1"
+        alt="1959 Austin Healy"
+        title="1959 Austin Healy"
         dayPrice="Coming Soon"
         go
         href="https://driveshare.com/collection/60921"
       />
       <ImageGridItem
-        src="/img/inventory/86 Bronco/86bronco1.jpg"
+        src="/img/car.svg"
         alt="1986 Ford Bronco"
         title="1986 Ford Bronco"
         dayPrice="Coming Soon"
