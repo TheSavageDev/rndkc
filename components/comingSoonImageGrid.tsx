120681c849ca08eb8f269352e9a5e440af79f893
Jason Savage <savage@revlogical.com>
2023-02-16 16:20:50 -0600

    More stuff

--- components/comingSoonImageGrid.tsx
+++ components/comingSoonImageGrid.tsx
@@ -4,7 +4,7 @@ export const ComingSoonImageGrid = () => {
   return (
     <section className="rental-ready_image-grid">
       <ImageGridItem
-        src="/img/car.svg"
+        src="https://firebasestorage.googleapis.com/v0/b/rndkc-95667.appspot.com/o/inventory%2F158S100419%2F4.JPG?alt=media&token=b9f87524-35d9-4b43-bf2a-a0fd260839f2"
         alt="1958 Chevy Corvette"
         title="1958 Chevy Corvette"
         go
@@ -12,7 +12,7 @@ export const ComingSoonImageGrid = () => {
         href="/car/158S100419"
       />
       <ImageGridItem
-        src="/img/inventory/59 Austin Healy/ah4.jpg"
+        src="https://firebasestorage.googleapis.com/v0/b/rndkc-95667.appspot.com/o/inventory%2FAN5L11026%2F1.jpg?alt=media&token=c9cae438-e198-4265-961a-9438ebd1f5f9"
         alt="1959 Austin Healy"
         title="1959 Austin Healy"
         dayPrice="Coming Soon"
@@ -20,11 +20,12 @@ export const ComingSoonImageGrid = () => {
         href="/car/AN5L11026"
       />
       <ImageGridItem
-        src="/img/car.svg"
+        src="https://firebasestorage.googleapis.com/v0/b/rndkc-95667.appspot.com/o/inventory%2F1FMDU15Y7GLA41918%2F1.JPG?alt=media&token=71be4c83-d325-41fc-81ac-9ae562e666e2"
         alt="1986 Ford Bronco"
         title="1986 Ford Bronco"
         dayPrice="Coming Soon"
         go
+        href="/car/1FMDU15Y7GLA41918"
         driveShare="https://driveshare.com/collection/60921"
       />
     </section>
--- components/imageGrid.tsx
+++ components/imageGrid.tsx
@@ -4,7 +4,7 @@ export const RentalReadyImageGrid = () => {
   return (
     <section className="rental-ready_image-grid">
       <ImageGridItem
-        src="/img/65plymouthSat.jpg"
+        src="https://firebasestorage.googleapis.com/v0/b/rndkc-95667.appspot.com/o/inventory%2FR451158693%2F1.jpg?alt=media&token=ed7fbce8-401c-4d03-a4d1-50ec816ae4e4"
         alt="1965 Plymouth Satellite"
         title="1965 Plymouth Satellite"
         dayPrice={302}
@@ -12,7 +12,7 @@ export const RentalReadyImageGrid = () => {
         driveShare="https://driveshare.com/car/1965-plymouth-satellite-9401"
       />
       <ImageGridItem
-        src="/img/69FordStang.jpg"
+        src="https://firebasestorage.googleapis.com/v0/b/rndkc-95667.appspot.com/o/inventory%2F9F02M126188%2F8.jpg?alt=media&token=c90fc0e0-4d7b-4583-b75f-6be8b010b929"
         alt="1969 Mach 1"
         title="1969 Mach 1"
         dayPrice={351}
@@ -20,7 +20,7 @@ export const RentalReadyImageGrid = () => {
         driveShare="https://driveshare.com/car/1969-ford-mustang-9312"
       />
       <ImageGridItem
-        src="/img/67ChevyChevelle.jpg"
+        src="https://firebasestorage.googleapis.com/v0/b/rndkc-95667.appspot.com/o/inventory%2F1341177132861%2F1.jpg?alt=media&token=95b68b73-64d6-46d6-93ad-1beb5756f3d3"
         alt="1967 Chevy Chevelle"
         title="1967 Chevy Chevelle"
         dayPrice={383}
