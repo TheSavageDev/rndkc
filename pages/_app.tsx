3b11f238507266f8ff889b93fbb4ae2e36b58cc8
Jason Savage <savage@revlogical.com>
2023-02-20 09:00:07 -0600

    Add types for window.gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -50,11 +50,8 @@ function MyApp({ Component, pageProps }: AppProps) {
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -62,9 +59,8 @@ function MyApp({ Component, pageProps }: AppProps) {
               page_path: window.location.pathname,
               send_page_view: false
             });
-          `,
-        }}
-      />
+          `}
+      </Script>
       {/* Global Site Code Pixel - Facebook Pixel */}
       <Script
         id="fb-pixel"
