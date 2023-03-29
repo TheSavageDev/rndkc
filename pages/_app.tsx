a1ef441e58b1869b169590d4c04faeab246aeb4e
Jason Savage <savage@revlogical.com>
2023-03-29 14:05:39 -0500

    Add pendo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,7 +19,7 @@ import * as fbq from "../lib/fpixel";
 import "../styles/globals.css";
 import "../styles/carPay.css";
 import "../styles/admin.css";
-import "../styles/calendar.css";
+// import "../styles/calendar.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
